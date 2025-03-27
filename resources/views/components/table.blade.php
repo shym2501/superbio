ccc5ff926a23e581ba1676658f526554c50d8896
Morten Bak <meb@indexed.dk>
2025-03-27 10:29:34 +0100

    add overflow scroll on table component

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,4 +1,4 @@
-<div class="overflow-hidden sm:rounded-lg mb-6 border border-zinc-200 dark:border-zinc-500 relative" wire:loading.class="opacity-50">
+<div class="overflow-hidden overflow-x-auto sm:rounded-lg mb-6 border border-zinc-200 dark:border-zinc-500 relative" wire:loading.class="opacity-50">
     <table class="min-w-full w-full text-zinc-500 dark:text-white/80">
         <thead class="text-xs uppercase bg-zinc-50 dark:bg-zinc-900 text-zinc-500 dark:text-white/80">
             <tr>
